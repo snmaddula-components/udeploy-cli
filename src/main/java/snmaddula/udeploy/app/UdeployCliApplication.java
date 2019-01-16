82f2f54a35eec708b40f542a229d0716901895dd
snmaddula <snmaddula@outlook.com>
2019-01-16 08:05:46 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/udeploy/app/UdeployCliApplication.java
@@ -0,0 +1,14 @@
+package snmaddula.udeploy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdeployCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdeployCliApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/snmaddula/udeploy/app/UdeployCliApplicationTests.java
@@ -0,0 +1,17 @@
+package snmaddula.udeploy.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdeployCliApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
